814d09bcb682d0272dba6d093d2a95a39bdb723a
biblebook00 <sgbb9052@gmail.com>
2024-07-25 15:42:29 +0900

    design : 카카오브레인블로그 클론코딩 완료

--- js/crew.js
+++ js/crew.js
@@ -1,25 +1,25 @@
 window.addEventListener("load", function () {
-  // 1. news.json 호출하고 성공하면
-  fetch("api/crews.json")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      const result = makeHtmlTag(data);
-      return result;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+ // 1. news.json 호출하고 성공하면
+ fetch("api/crews.json")
+  .then((response) => {
+   return response.json();
+  })
+  .then((data) => {
+   const result = makeHtmlTag(data);
+   return result;
+  })
+  .catch((error) => {
+   console.log(error);
+  });
 
-  // 2. html 태그 생성 함수
-  const makeHtmlTag = (data) => {
-    // html 태그 글자
-    let tag = "";
-    // 이미지 경로
-    const path = "./images";
-    data.forEach((item) => {
-      const tempTag = `
+ // 2. html 태그 생성 함수
+ const makeHtmlTag = (data) => {
+  // html 태그 글자
+  let tag = "";
+  // 이미지 경로
+  const path = "./images";
+  data.forEach((item) => {
+   const tempTag = `
     <a href="${item.link}" class="content-list-link" data-id="${item.id}">
         <div class="content-list-img">
            <div class="content-list-thumb" style="background: url('${path}/${item.imgpath}') no-repeat center;
@@ -35,12 +35,12 @@ window.addEventListener("load", function () {
         </div>
     </a>`;
 
-      tag += tempTag;
-    });
+   tag += tempTag;
+  });
 
-    // html 배치
-    const crewElement = document.querySelector(".crew-list");
-    crewElement.innerHTML = tag;
-    return crewElement;
-  };
+  // html 배치
+  const crewElement = document.querySelector(".crew-list");
+  crewElement.innerHTML = tag;
+  return crewElement;
+ };
 });
